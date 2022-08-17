36591899b8beba6c9d47af5fae5997c37c82b852
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-17 22:49:44 +0300

    Using okhttp3 log Interceptor

--- app/src/main/java/com/example/connecttointernet/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/MainActivity.kt
@@ -5,20 +5,24 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.connecttointernet.data.NotionalResponse
 import com.example.connecttointernet.databinding.ActivityMainBinding
-import com.example.connecttointernet.util.RequestsInterceptor
 import com.google.gson.Gson
 import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private val client = OkHttpClient.Builder().addInterceptor(RequestsInterceptor()).build()
+    private val logInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    private val client = OkHttpClient.Builder().addInterceptor(logInterceptor).build()
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-        makeHttpRequest("ahmed")
-        makeHttpRequest("smeath")
+        setContentView(binding.root)
+
+        binding.btnFetch.setOnClickListener {
+            makeHttpRequest(binding.etName.text.toString())
+        }
     }
 
     private fun makeHttpRequest(name: String) {
