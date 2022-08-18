dc034c94d2b05ff3a769970b1f322f8ee7612c10
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-18 13:48:18 +0300

    (Just) operator

--- app/src/main/java/com/example/connecttointernet/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.connecttointernet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.connecttointernet.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Observable
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        foo()
+    }
+
+    private fun foo() {
+        val observable = Observable.just(1, 5, 7, 10, 12)
+
+        observable.subscribe(
+            { t -> Log.d(TAG, "foo: $t") },
+            { e -> Log.d(TAG, "foo: ${e.message}") }
+        )
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
