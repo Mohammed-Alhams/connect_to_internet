89f7948bb6a3db482c4a42dc81ffe90a2816dd16
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-19 18:18:36 +0300

    Async Subject

--- app/src/main/java/com/example/connecttointernet/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.connecttointernet.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.AsyncSubject
 import io.reactivex.rxjava3.subjects.PublishSubject
 import java.util.concurrent.TimeUnit
 
@@ -26,21 +27,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun foo() {
-//        val observable = Observable.interval(1, TimeUnit.SECONDS)
+        val observable = Observable.interval(1, TimeUnit.SECONDS).take(5)
 
-        val subject = PublishSubject.create<Int>()
-//        observable.subscribe(subject)
+        val subject = AsyncSubject.create<Long>()
+        observable.subscribe(subject)
 
-//        Thread.sleep(5000)
+        Thread.sleep(5000)
 
-        subject.onNext(100)
-        subject.onNext(102)
         subject.subscribe(
             {t -> Log.d(TAG, "foo: $t")},
             {e -> }
         )
-        subject.onNext(104)
-        subject.onNext(106)
     }
 
     override fun onDestroy() {
