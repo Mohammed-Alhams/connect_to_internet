bb73b44c95b3fbc1148faa35ae4d6322079d275a
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-19 21:08:51 +0300

    coroutines scope, dispatcher and builder

--- app/src/main/java/com/example/connecttointernet/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.connecttointernet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.connecttointernet.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        GlobalScope/*coroutines scope*/.launch(Dispatchers.IO/*coroutines dispatcher*/)
+        {//coroutines builder
+            fakeRequest()
+        }
+        Log.d(TAG, "onCreate: after request")
+    }
+
+    suspend fun fakeRequest() {
+        delay(3000)
+        Log.d(TAG, "fakeRequest: result")
+        Log.d(TAG, "fakeRequest: ${Thread.currentThread().name}")
+        withContext(Dispatchers.Main) {
+            binding.tvText.text = "fakeRequest"
+            Log.d(TAG, "fakeRequest: ${Thread.currentThread().name}")//context switching
+        }
+    }
+
+    companion object {
+        private const val TAG = "co-alhams"
     }
 }
