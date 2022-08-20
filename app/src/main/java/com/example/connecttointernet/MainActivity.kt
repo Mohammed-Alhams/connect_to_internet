1e3779945fdd9c34dbf5187d108ec03a1c17e827
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-20 23:06:01 +0300

    add required library's dependencies

--- app/src/main/java/com/example/connecttointernet/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.connecttointernet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.connecttointernet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
