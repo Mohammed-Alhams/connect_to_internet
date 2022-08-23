af1c3faeb7caff13b5aaac2b5aed3697140cb311
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-23 12:45:51 +0300

    Encapsulate Live Data

--- app/src/main/java/com/example/connecttointernet/ui/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/ui/MainActivity.kt
@@ -3,16 +3,13 @@ package com.example.connecttointernet.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.connecttointernet.databinding.ActivityMainBinding
-import com.example.connecttointernet.model.domain.User
 import com.example.connecttointernet.viewModels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    private val viewModel : MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +18,15 @@ class MainActivity : AppCompatActivity() {
         setup()
     }
 
-    private fun setup(){
+    private fun setup() {
         viewModel.getUserInfo()
         binding.btnGenerate.setOnClickListener { viewModel.getWisdom() }
 
-        viewModel.currentUser.observe(this
+        viewModel.currentUser.observe(
+            this
         ) { binding.tvName.text = it.userName }
 
-        viewModel.wisdom.observe(this){
+        viewModel.wisdom.observe(this) {
             binding.tvWisdomDate.text = it.publishDate
             binding.tvWisdomContent.text = it.content
         }
