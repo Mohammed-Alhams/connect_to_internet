f8d15d74c138456bdb43c44d180a31ffda314bf9
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-24 21:10:05 +0300

    Retrofit with coroutines

--- app/src/main/java/com/example/connecttointernet/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/connecttointernet/viewModels/MainViewModel.kt
@@ -2,10 +2,12 @@ package com.example.connecttointernet.viewModels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.connecttointernet.API
 import com.example.connecttointernet.model.JokeApiResponse
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
@@ -17,17 +19,10 @@ class MainViewModel : ViewModel() {
     }
 
     private fun getRandomJoke() {
-        API.apiService.getRandomJoke()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                {
-                    joke.postValue(it)
-                },
-                {
+        viewModelScope.launch {
+            joke.postValue(API.apiService.getRandomJoke())
+        }
 
-                }
-            )
     }
 
 }
