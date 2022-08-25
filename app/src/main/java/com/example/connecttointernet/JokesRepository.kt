375505686b31e29e7313deec23a3044cc06599ec
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-25 18:56:25 +0300

    7- Create a state wrapper function

--- app/src/main/java/com/example/connecttointernet/JokesRepository.kt
+++ app/src/main/java/com/example/connecttointernet/JokesRepository.kt
@@ -5,22 +5,27 @@ import com.example.connecttointernet.model.State
 import com.example.connecttointernet.networking.API
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.Response
 
 class JokesRepository {
     /*suspend can be omitted suspend keyword */
     fun getRandomJoke(): Flow<State<JokeApiResponse?>> /*Using flow we can omit multiple statuses of response*/ {
+        return wrapWithFlow(API.apiService::getRandomJoke)
+    }
+
+    fun <T> wrapWithFlow(function: suspend () -> Response<T>): Flow<State<T?>> {
         return flow {
             emit(State.Loading)
             try {
-                val response = API.apiService.getRandomJoke()
-                if (response.isSuccessful) {
+                val response = function()
+                if (response.isSuccessful)
                     emit(State.Success(response.body()))
-                } else {
-                    emit(State.Failed(response.message()))
-                }
+                else
+                    emit(State.Failed(response.message().toString()))
             } catch (e: Exception) {
                 emit(State.Failed(e.message.toString()))
             }
         }
     }
+
 }
