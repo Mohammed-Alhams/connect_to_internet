ca1a922522ed6501b3c76d3e0b856d3533ca4b91
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-25 19:16:10 +0300

    8- Adding logging interceptor

--- app/src/main/java/com/example/connecttointernet/networking/API.kt
+++ app/src/main/java/com/example/connecttointernet/networking/API.kt
@@ -1,14 +1,19 @@
 package com.example.connecttointernet.networking
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object API {
 
+    private val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+    private val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
     private val BASE_URL = "https://v2.jokeapi.dev/"
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
