4c1ecf8bf943059b5b3b779f6f27e480579544c1
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-25 14:17:03 +0300

    6- Switch to detaining

--- app/src/main/java/com/example/connecttointernet/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.connecttointernet
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.connecttointernet.databinding.ActivityMainBinding
+import com.example.connecttointernet.viewModels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private val viewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
     }
 }
