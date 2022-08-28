789e0ce2ede925d0851dc8a5cc4ad343a300e4f0
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-28 21:22:29 +0300

    Integrate action bar with navigation controller

--- app/src/main/java/com/example/connecttointernet/ui/MainActivity.kt
+++ app/src/main/java/com/example/connecttointernet/ui/MainActivity.kt
@@ -3,16 +3,30 @@ package com.example.connecttointernet.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.connecttointernet.R
 import com.example.connecttointernet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        navController = findNavController(R.id.fragment_container)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return true
     }
 }
