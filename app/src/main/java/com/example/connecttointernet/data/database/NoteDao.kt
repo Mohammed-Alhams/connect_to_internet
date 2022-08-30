d91f5b8968ff0431339b0d072f1023160fa16900
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-30 17:08:46 +0300

    Note Dao

---
+++ app/src/main/java/com/example/connecttointernet/data/database/NoteDao.kt
@@ -0,0 +1,21 @@
+package com.example.connecttointernet.data.database
+
+import androidx.room.*
+import com.example.connecttointernet.data.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    fun insertNote(note: Note)
+
+    @Delete
+    fun deleteNote(note: Note)
+
+    @Update
+    fun updateNote(note: Note)
+
+    @Query("SELECT * FROM Note")
+    fun getAllNotes(note: Note)
+
+}
