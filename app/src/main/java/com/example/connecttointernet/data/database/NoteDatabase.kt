0d69284430e0228b75051ed8e4d6708937006a58
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-08-31 22:42:11 +0300

    Automated migration in Room Database

--- app/src/main/java/com/example/connecttointernet/data/database/NoteDatabase.kt
+++ app/src/main/java/com/example/connecttointernet/data/database/NoteDatabase.kt
@@ -1,15 +1,14 @@
 package com.example.connecttointernet.data.database
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.*
 import com.example.connecttointernet.data.Note
 
-@Database(entities = [Note::class], version = 2)
+@Database(
+    entities = [Note::class],
+    version = 2,
+    autoMigrations = [AutoMigration(from = 1, to = 2)]
+)
 @TypeConverters(Converters::class)
 abstract class NoteDatabase : RoomDatabase() {
 
@@ -26,7 +25,7 @@ abstract class NoteDatabase : RoomDatabase() {
             Room.databaseBuilder(context, NoteDatabase::class.java, DATABASE_NAME)
                 /**.fallbackToDestructiveMigration()**///Allows Room to destructively recreate database
 //                tables if Migrations that would migrate old database schemas to the latest schema version are not found.
-                .addMigrations(MIGRATION_1_2)
+                /**.addMigrations(MIGRATION_1_2)**/
                 .build()
 
         fun getInstance(context: Context) =
@@ -34,11 +33,11 @@ abstract class NoteDatabase : RoomDatabase() {
 
         fun getInstanceWithoutContext() = instance!!
 
-        private val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE Note ADD COLUMN archived INTEGER NOT NULL DEFAULT 0")
-            }
-        }
+//        private val MIGRATION_1_2 = object : Migration(1, 2) {
+//            override fun migrate(database: SupportSQLiteDatabase) {
+//                database.execSQL("ALTER TABLE Note ADD COLUMN archived INTEGER NOT NULL DEFAULT 0")
+//            }
+//        }
 
     }
 
